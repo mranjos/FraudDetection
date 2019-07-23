b658b7bd6d62d6c4c31ae535ab08915f08f8d574
Michelangelo <mranjos91@gmail.com>
2019-07-23 16:13:52 -0300

    Leitura dos dados

--- Desenvolvimento.R
+++ Desenvolvimento.R
@@ -2,3 +2,11 @@
 ####     KAGGLE FRAUD DETECTION    ####
 ####          Mika & WELL          ####
 #######################################
+
+library(dplyr)
+library(data.table)
+
+train_id = fread("G:\\Meu Drive\\BACKUP_LENOVO\\D\\Michelangelo\\GitHub\\Dados\\Treino\\train_identity.csv")
+train_tr = fread("G:\\Meu Drive\\BACKUP_LENOVO\\D\\Michelangelo\\GitHub\\Dados\\Treino\\train_transaction.csv")
+test_id  = fread("G:\\Meu Drive\\BACKUP_LENOVO\\D\\Michelangelo\\GitHub\\Dados\\Teste\\test_identity.csv")
+test_tr  = fread("G:\\Meu Drive\\BACKUP_LENOVO\\D\\Michelangelo\\GitHub\\Dados\\Teste\\test_transaction.csv")
